87deb42704d1319a750ca56a19740f3107b3eb24
Davi95-BR <74278382+Davi95-BR@users.noreply.github.com>
2021-03-22 19:23:55 -0300

    Delete orientacaoObjetos directory

--- orientacaoObjetos/Funcionarios/Diretor.js
+++
@@ -1,9 +0,0 @@
-import { Funcionario } from "./Funcionario.js"
-
-
-export class Diretor extends Funcionario {
-	constructor(nome, salario, cpf){
-		super(nome, salario, cpf);// Chamando a classe superior Funcionario
-		this._bonificacao = 2;
-	}
-}
